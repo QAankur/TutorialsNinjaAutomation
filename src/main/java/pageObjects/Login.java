fd7247573866c12fe67cc662cf65b86658dd25a5
Ankur <ankur.mishra671@gmail.com>
2026-05-21 23:13:53 +0530

    updatedChanges

--- src/main/java/pageObjects/Login.java
+++ src/main/java/pageObjects/Login.java
@@ -18,7 +18,7 @@ public class Login {
 	private WebElement emailField;
 	
 	@FindBy(xpath="//input[@id='input-password']")
-	private WebElement pwdFieldThisIsRemoteChange;
+	private WebElement pwdFieldAndThisismyChange;
 	
 	@FindBy(xpath="//input[@value='Login']")
 	private WebElement loginBtnTypeloginbuttonAndRemain;
